7b74397b87b26b089a166efb4e288158088988db
Lesley <tomosadalesley@gmail.com>
2022-08-24 18:48:56 -0700

    completed step model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-
 from recipes.models import Recipe
+from recipes.models import Measure
+from recipes.models import FoodItem
+from recipes.models import Ingredient
+from recipes.models import Step
 
 admin.site.register(Recipe)
 
+admin.site.register(Measure)
+
+admin.site.register(FoodItem)
+
+admin.site.register(Ingredient)
+
+admin.site.register(Step)
 # Register your models here.
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 
@@ -11,3 +12,38 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name + " by " + self.author
+
+
+class Measure(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    abbreviation = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    amount = models.FloatField()
+    recipe = models.ForeignKey(
+        "Recipe", related_name="ingredients", on_delete=models.CASCADE
+    )
+    measure = models.ForeignKey("Measure", on_delete=models.CASCADE)
+    food = models.ForeignKey("FoodItem", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.amount} {self.measure} {self.food}"
+
+
+class Step(models.Model):
+    recipe = models.ForeignKey(
+        "Recipe", related_name="steps", on_delete=models.CASCADE
+    )
+    order = models.SmallIntegerField()
+    directions = models.CharField(max_length=300)
