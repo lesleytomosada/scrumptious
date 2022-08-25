3284a53ab0f9ca255804947f209fc5dffc0f99b5
Lesley <tomosadalesley@gmail.com>
2022-08-24 19:19:14 -0700

    added tags

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    recipes = models.ForeignKey(
+        "recipes.Recipe", related_name="tags", on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
