794b0f9210a3ebe3991c7bad67ed553e7e866d4b
Lesley <tomosadalesley@gmail.com>
2022-08-29 16:31:53 -0700

    converted list function to list class

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.views.generic.list import ListView
 
 from recipes.forms import RatingForm
 
@@ -44,13 +45,6 @@ def change_recipe(request, pk):
     return render(request, "recipes/edit.html", context)
 
 
-def show_recipes(request):
-    context = {
-        "recipes": Recipe.objects.all() if Recipe else [],
-    }
-    return render(request, "recipes/list.html", context)
-
-
 def show_recipe(request, pk):
     context = {
         "recipe": Recipe.objects.get(pk=pk) if Recipe else None,
@@ -67,3 +61,8 @@ def log_rating(request, recipe_id):
             rating.recipe = Recipe.objects.get(pk=recipe_id)
             rating.save()
     return redirect("recipe_detail", pk=recipe_id)
+
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = "recipes/list.html"
